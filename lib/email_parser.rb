f32c8b494c66940fbaa846a038d6e9b9a5013e1f
Lorena <lorena.giddens@outlook.com>
2018-06-13 17:49:42 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end
 
   def parse
-    emails.split(/[" "]/).select {|email| email.length > 1}.uniq
+    emails.split(/[" ",]/).select {|email| email.length > 1}.uniq
   end
 
 end
